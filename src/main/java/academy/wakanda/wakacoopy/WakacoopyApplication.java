e16516f5f39e91376c627f23e1f3af338ee51c6a
Jane Gomes <asscontecjane@gmail.com>
2024-12-03 17:47:27 -0300

    init

---
+++ src/main/java/academy/wakanda/wakacoopy/WakacoopyApplication.java
@@ -0,0 +1,13 @@
+package academy.wakanda.wakacoopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakacoopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakacoopyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/wakanda/wakacoopy/WakacoopyApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.wakanda.wakacoopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakacoopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
